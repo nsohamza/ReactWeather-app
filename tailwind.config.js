35f3111f92498412f2e44f64c3e9d9ebf4257ebf
Ebrima Conta <ebrimaconta@gmail.com>
2025-04-21 13:45:30 +0100

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
